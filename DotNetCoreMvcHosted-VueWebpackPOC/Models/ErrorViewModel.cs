b67faecbd9b46bd51d55f2ab33bcb1fd7c4da4b6
Steve Tipton <slaatipton26@gmail.com>
2018-06-12 12:36:42 -0400

    Add initial dotnet new mvc structure

---
+++ DotNetCoreMvcHosted-VueWebpackPOC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotNetCoreMvcHosted_VueWebpackPOC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
