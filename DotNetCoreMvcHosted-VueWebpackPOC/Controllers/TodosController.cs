26860ccbc687a52e898b343848c758890a9dee74
Steve Tipton <slaatipton26@gmail.com>
2018-07-05 11:01:18 -0400

    Fix TodosController to return fake file-based json from PhysicalPath

--- DotNetCoreMvcHosted-VueWebpackPOC/Controllers/TodosController.cs
+++ DotNetCoreMvcHosted-VueWebpackPOC/Controllers/TodosController.cs
@@ -1,20 +1,36 @@
-using System;
 using System.IO;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
 
 namespace DotNetCoreMvcHosted_VueWebpackPOC.Controllers
 {
   [Produces("application/json")]
+  [Route("api/[controller]")]
   public class TodosController : Controller
   {
+    private readonly IHostingEnvironment _hostingEnvironment;
+
+    public TodosController(IHostingEnvironment hostingEnvironment)
+    {
+      _hostingEnvironment = hostingEnvironment;
+    }
+
+    // GET api/values
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+      return new string[] { "Continue reading A Storm of Swords", "Nerd out on r/freefolk", "Learn Dothraki" };
+    }
+
     // GET api/FakeTodos
     [HttpGet]
-    [Route("~api/FakeTodos")]
-    public JsonResult GetFakeTodos()
+    [Route("faketodos")]
+    public IActionResult GetFakeTodos()
     {
-      var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "todos.json");
-
-      return Json(path);
+      var contentRootPath = _hostingEnvironment.ContentRootPath;
+      var path = Path.Combine(contentRootPath, "Data", "todos.json");
+      return PhysicalFile(path, "application/json");
     }
   }
 }
